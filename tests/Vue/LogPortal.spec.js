fcbad0e45a88e7d2cc9993bbc71886b955d58c6b
paulappz <pauloluyege@gmail.com>
2019-05-10 09:25:05 +0100

    set-up @paulappz

---
+++ tests/Vue/LogPortal.spec.js
@@ -0,0 +1,25 @@
+import { shallow, mount } from 'vue-test-utils';
+import expect from 'expect';
+// import Brand from '../../resources/assets/js/views/LOG/brand/form.vue';
+import moxios from 'moxios'
+import Vue from "vue";
+// import VeeValidate from 'vee-validate';
+
+// Vue.use(VeeValidate);
+
+// describe('Brand', () => {
+//   beforeEach (() => {
+//     moxios.install(axios);
+//   });
+
+//   afterEach (() => {
+//     moxios.uninstall(axios);
+//   });
+
+//   it('renders the correct title on the page', () => {
+//     let wrapper = shallow(Brand);
+
+//     expect(wrapper.html()).toContain('Brand Details');
+// });
+
+// });
---
+++ tests/Vue/setup.js
@@ -0,0 +1,14 @@
+// require('jsdom-global')();
+
+global.expect = require('expect')
+global.axios = require('axios');
+global.Vue = require('vue');
+global.bus = new Vue();
+
+// from @vue/cli-plugin-unit-mocha/setup.js
+require('jsdom-global')(undefined, { pretendToBeVisual: true, url: 'http://localhost' })
+
+// https://github.com/vuejs/vue-test-utils/issues/936
+// better fix for "TypeError: Super expression must either be null or
+// a function" than pinning an old version of prettier.
+window.Date = Date
